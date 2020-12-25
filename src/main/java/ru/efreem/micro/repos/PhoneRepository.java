2e949c603682b86842171b6589844685c85833cb
Efreem <truecybersec@gmail.com>
2020-12-26 01:59:06 +0600

    Created update query for User

--- src/main/java/ru/efreem/micro/repos/PhoneRepository.java
+++ src/main/java/ru/efreem/micro/repos/PhoneRepository.java
@@ -1,9 +1,16 @@
 package ru.efreem.micro.repos;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.efreem.micro.model.Phone;
 
+import java.math.BigDecimal;
+
 @Repository
 public interface PhoneRepository extends CrudRepository<Phone, Long> {
+    @Modifying
+    @Query(value = "UPDATE User u SET u.cash=?1 WHERE u.id=?2")
+    void updateUserCashById(BigDecimal cash, Long id);
 }

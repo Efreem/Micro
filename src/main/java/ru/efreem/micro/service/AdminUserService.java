b2f9b65ca3490735747f9050cf65381124f211cc
Efreem <truecybersec@gmail.com>
2020-12-26 02:41:36 +0600

    Created AdminUserService.java

---
+++ src/main/java/ru/efreem/micro/service/AdminUserService.java
@@ -0,0 +1,53 @@
+package ru.efreem.micro.service;
+
+import org.graalvm.compiler.lir.LIRInstruction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.efreem.micro.model.Profile;
+import ru.efreem.micro.model.User;
+import ru.efreem.micro.repos.ProfileRepository;
+import ru.efreem.micro.repos.UserRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class AdminUserService implements AdminService, DefaultService {
+    private UserRepository userRepository;
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    public AdminUserService(UserRepository userRepository, ProfileRepository profileRepository) {
+        this.userRepository = userRepository;
+        this.profileRepository = profileRepository;
+    }
+
+    public void updateProfileCashById(BigDecimal cash, Long id) {
+
+    }
+
+    public List<User> getUserByGreaterAge(Byte age) {
+        return null;
+    }
+
+    public List<User> getUserByUnderAge(Byte age) {
+        return null;
+    }
+
+    public List<User> getUserByAge(Byte age) {
+        return null;
+    }
+
+    public List<User> getUserByPhone(String phone) {
+        return null;
+    }
+
+    public List<User> getUserByName(String name) {
+        return null;
+    }
+
+    public List<User> getUserByEmail(String email) {
+        return null;
+    }
+
+}
